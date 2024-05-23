87ae198b6ebf1587d80fa467fafd19110a34795f
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-23 13:21:41 +0000

    set to GET only

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ export default {
     const key = url.pathname.slice(1); // Extract key from URL path
 
     switch (request.method) {
-      case "PUT":
-        // PUT request: Store the image in KV store
-        await env.IMAGES.put(key, request.body);
-        return new Response(`Put ${key} successfully!`, { status: 201 });
+      // case "PUT":
+      //   // PUT request: Store the image in KV store
+      //   await env.IMAGES.put(key, request.body);
+      //   return new Response(`Put ${key} successfully!`, { status: 201 });
 
       case "GET":
         // GET request: Retrieve the image from KV store
@@ -26,17 +26,17 @@ export default {
         // Return the image with appropriate headers
         return new Response(object.body, { headers });
 
-      case "DELETE":
-        // DELETE request: Remove the image from KV store
-        await env.IMAGES.delete(key);
-        return new Response("Deleted!", { status: 200 });
+      // case "DELETE":
+      //   // DELETE request: Remove the image from KV store
+      //   await env.IMAGES.delete(key);
+      //   return new Response("Deleted!", { status: 200 });
 
       default:
         // Unsupported method
         return new Response("Method Not Allowed", {
           status: 405,
           headers: {
-            Allow: "PUT, GET, DELETE",
+            Allow: "GET",
           },
         });
     }
